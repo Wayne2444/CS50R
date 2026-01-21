2c92c1f5885300a5af0c8ecaee359fb5fb29cfe8
Wayne2444 <13319272819wayne@gmail.com>
2026-01-21 15:36:11 -0600

    Add files via upload

---
+++ Problem Set 4/air/zelda/5.R
@@ -0,0 +1,15 @@
+load("zelda.RData")
+
+zelda <- zelda |>
+  mutate(
+    year = as.integer(year),
+    system = str_squish(system),
+    title  = str_squish(title)
+  ) |>
+  filter(str_detect(producers, ",")) |>
+  group_by(title) |>
+  slice_min(order_by = year, n=1, with_ties = TRUE) |>
+  ungroup() |>
+  arrange(year, title, system)
+
+save(zelda, file = "5.RData")

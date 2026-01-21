6a3942ed2052ba1f77bf03e8198c6038f0f16a78
Wayne2444 <13319272819wayne@gmail.com>
2026-01-21 15:37:02 -0600

    Delete Problem Set 4/air directory

--- Problem Set 4/air/4.R
+++
@@ -1,9 +0,0 @@
-load("air.RData")
-
-air$emissions <- as.numeric(gsub(",", "", air$emissions))
-
-air <- air |>
-  filter (county == "OR - Lane") |>
-  arrange (desc(emissions))
-
-save(air, file = "4.RData")
--- Problem Set 4/air/zelda/2.R
+++
@@ -1,9 +0,0 @@
-load("zelda.RData")
-
-zelda <- zelda |>
-  group_by(year) |>
-  rename(releases = title) |>
-  summarize(releases = n()) |>
-  arrange(desc(releases))
-
-save(zelda, file = "2.RData")

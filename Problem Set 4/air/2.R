482018ec60d609d54948ceafdd95327e2ce4761b
Wayne2444 <13319272819wayne@gmail.com>
2026-01-20 19:16:15 -0600

    Delete Problem Set 4/air directory

--- Problem Set 4/air/2.R
+++
@@ -1,8 +0,0 @@
-load("air.RData")
-
-air$emissions <- as.numeric(gsub(",", "", air$emissions))
-
-air <- air |>
-  arrange(desc(emissions))
-
-save(air, file = "2.RData")
--- Problem Set 4/air/3.R
+++
@@ -1,6 +0,0 @@
-load("air.RData")
-
-air <- air |>
-  filter(county == "OR - Lane")
-
-save(air, file = "3.RData")
